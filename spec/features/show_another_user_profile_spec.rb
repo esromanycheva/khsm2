73271bff4d4706fb5a13d228c1990cc4483ecf45
esromanycheva <94757683+esromanycheva@users.noreply.github.com>
2022-07-16 12:17:11 +0300

    Add capibara Show another user profile

---
+++ spec/features/show_another_user_profile_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature 'Show another user profile', type: :feature do
+  let(:user) { create :user }
+  let(:another_user) { create :user }
+
+  let(:game) { create(:game_with_questions, user: another_user, current_level: 5) }
+
+  before do
+    game.send(:finish_game!, 1000, false)
+    login_as user
+  end
+
+  scenario 'successfully' do
+    visit "/users/#{another_user.id}"
+
+    expect(page).to have_content "#{another_user.name}"
+    expect(page).to have_content '1 000 ₽'
+  end
+end
